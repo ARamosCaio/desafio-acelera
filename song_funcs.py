4cf12317bcae5a863fbe18375b03f185a2d1c029
Caio Ramos <aramoscaio@gmail.com>
2023-08-08 22:41:11 -0300

    feat: Added file to store the song lyrics

---
+++ song_funcs.py
@@ -0,0 +1,4 @@
+def song(n, ni):
+    print(f'{n} patinhos foram passear \nAlém das montanhas \nPara brincar\nA mamãe gritou: Quá, quá, quá, quá\nMas só {n-1} patinhos voltaram de lá')
+    if n == 1:
+        print(f'A mamãe patinha foi procurar\nAlém das montanhas\nNa beira do mar\nA mamãe gritou: Quá, quá, quá, quá\nE os {ni} patinhos voltaram de lá')
