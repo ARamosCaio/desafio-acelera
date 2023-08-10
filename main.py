9586c81cc179b10deb96879bcaa6169480e09ff9
Caio Ramos <aramoscaio@gmail.com>
2023-08-09 22:55:36 -0300

    feat: Added a 2 second delay after each verse and the clean_verse function

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from song_funcs import song
+from song_funcs import clean_verse
+import time
+import sys
 
 ni = int(input('Insira o número de patinhos: '))
 n = ni
-while n > 0:
+while n >= 0:
     song(n, ni)
+    time.sleep(2)
     n-=1
+    clean_verse()
+
+    
